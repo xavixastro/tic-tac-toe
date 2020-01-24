272d79cfa3fe483724a9a6739585d2c8ae587e68
javier <xavixastro@gmail.com>
2020-01-24 14:08:28 -0800

    board class and grid functionality

---
+++ board.rb
@@ -0,0 +1,53 @@
+class Board
+
+    def initialize
+        @grid = Array.new(3) {Array.new(3, "_")}    
+    end
+
+    def valid?(position)
+        row, col = position
+        (0...@grid.length).include?(row) && (0...@grid.length).include?(col)
+    end
+
+    def empty?(position)
+        row, col = position
+        @grid[row][col] == "_"
+    end
+
+    def place_mark(position, mark)
+        if self.valid?(position) && self.empty?(position)
+            row, col = position
+            @grid[row][col] = mark
+        else
+            raise "Invalid position"
+        end
+    end
+
+    def print
+        @grid.each do |row|
+            puts "#{row.join(" ")}"
+        end
+    end
+
+    def win_row?(mark)
+        @grid.any?{|row| row.all?{|el| el == mark}}
+    end
+
+    def win_col?(mark)
+        @grid.transpose.any?{|row| row.all?{|el| el == mark}}
+    end
+
+    def win_diagonal?(mark)
+        (0..@grid.length-1).all? {|idx| @grid[idx][idx] == mark } ||
+        (0..@grid.length-1).all? {|idx| @grid[idx][-1-idx] == mark } 
+    end
+
+    def win?(mark)
+        win_row?(mark) || win_col?(mark) || win_diagonal?(mark)
+    end
+
+    def empty_positions?
+        @grid.any?{|row| row.any?{|el| el == "_"}}
+    end
+
+end
